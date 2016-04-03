f0c9d8688c1c4ffdc2e55c2c927c989da22bce20
Lyubomyr Fedevitch <lmfedevitch@gmail.com>
2016-04-03 17:21:08 +0300

    added ukrainian locale

---
+++ src/locale/uk_UA.js
@@ -0,0 +1,8 @@
+import ukUA from 'gregorian-calendar-format/lib/locale/uk_UA';
+import ukUaCalendar from 'gregorian-calendar/lib/locale/uk_UA';
+
+export default {
+  clear: 'Очистити',
+  format: ukUa,
+  calendar: ukUaCalendar,
+};
